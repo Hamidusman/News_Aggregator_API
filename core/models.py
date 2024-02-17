4fce09830aedb1561aa0d73b0eb4a149be5853a5
Hamidusman <abdulhamidusman218@gmail.com>
2024-02-17 09:39:48 +0000

    ..

--- core/models.py
+++ core/models.py
@@ -15,7 +15,7 @@ class News(models.Model):
     title = models.CharField(max_length = 100)
     content = models.CharField(max_length= 100000)
     categoy = models.ChoiceField(category, choices=category)
-    created_at = models.datetime(auto_now=True)
+    created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
